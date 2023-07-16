6a85af1f6c2b65397acd3d2cd9340e957f79ab2d
Ritu Mishra <ritu8924mishra@gmail.com>
2023-07-16 13:24:07 +0530

    .Net Lab Exam Commit

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DaysOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = new string[]
+             {
+            "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+             };
+
+            var daysQuery = from day in daysOfWeek
+                            select day;
+
+            foreach (var day in daysQuery)
+            {
+                Console.WriteLine(day);
+            }
+        }
+}
+}
